e93a6590153d7066d9237611408e3770ede862fa
Gustavo Veloso <gustavovv3@gmail.com>
2023-09-27 20:20:27 -0300

    pagina de login

--- Frontend/doctorsync/app/login/page.tsx
+++ Frontend/doctorsync/app/login/page.tsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function pages() {
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <div className='bg-darkgreen text-primary w-auto p-8'>
-        TO NO CENTRO
+    <div className="flex items-center justify-center h-screen bg-primary">
+      <div className="flex bg-login text-primary w-auto p-8">
+        <div className="flex-col w-2/3 mr-8">
+          <div className="flex-col justify-center items-center text-center">
+            <p className="text-xl font-bold">Login</p>
+            <p className="text-xs text-cinzaclaro">Bem vindo de volta</p>
+          </div>
+          <div className=" flex justify-start w-full ml-4 mt-4">
+            <form>
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold ml-2 text-xxs"
+                >
+                  EMAIL *:
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  className="border rounded-lg px-3 py-2 bg-secundary w-full"
+                  placeholder="Digite seu email"
+                />
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold ml-2 mt-4 text-xxs"
+                >
+                  SENHA *:
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  className="border rounded-lg px-3 py-2 bg-secundary w-full"
+                  placeholder="Digite sua senha"
+                />
+              <button
+                type="submit"
+                className="bg-green hover:bg-blue text-white font-bold  mt-4 py-2 px-4 rounded w-full mt-8"
+              >
+                Log In
+              </button>
+              <a className="mt-8">Criar nova conta</a>  
+            </form>
+          </div>
+        </div>
+        <div className="w-1/3 bg-gray">
+          <Image
+            src={"/login.png"}
+            alt="My Image"
+            width={400} // Set the width of the image
+            height={341} // Set the height of the image
+          />
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
--- Frontend/doctorsync/tailwind.config.ts
+++ Frontend/doctorsync/tailwind.config.ts
@@ -14,7 +14,20 @@ const config: Config = {
       gray: '#767676',
       darkgreen: '#354F52',
       green: '#52796F',
-      black: '#000000'
+      black: '#000000',
+      cinzaclaro: '#B5BAC1',
+      login:'#2F3E46'
+    },
+    fontSize : {
+      xxs: ['10px', { lineHeight: '24px', letterSpacing: ' -0.03em' }],
+      xs: ['14px', { lineHeight: '24px', letterSpacing: ' -0.03em' }], 
+      sm: ['16px', { lineHeight: '28px', letterSpacing: ' -0.03em' }], 
+      lg: ['18px', { lineHeight: '28px', letterSpacing: '-0.03em' }], 
+      xl: ['24px', { lineHeight: '36px', letterSpacing: ' -0.03em' }], 
+      '2x1': ['36px', { lineHeight: '48px', letterSpacing: '-0.032em' }], 
+      '3x1': ['48px', { lineHeight: '56px', letterSpacing: '-0.032em' }],
+      '4x1': ['56px', { lineHeight: '64px', letterSpacing: '-0.032em' }],
+      '5x1': ['80px', { lineHeight: '80px', letterSpacing: '-0.032em' }]
     },
     extend: {},
   },
