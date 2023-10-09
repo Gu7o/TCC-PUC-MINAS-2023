8e69a35e16bf4ca2a250ad60270e8e82e2d9796d
Gustavo Veloso <gustavovv3@gmail.com>
2023-10-08 21:49:38 -0300

    ajustando componentes de busca de pacientes

--- Frontend/doctorsync/app/pacientes/page.tsx
+++ Frontend/doctorsync/app/pacientes/page.tsx
@@ -4,10 +4,18 @@ import Paciente from '@/components/paciente'
 
 export default function page() {
   return (
-    <div className='flex bg-green'>
+    <div className='flex bg-green w-full'>
         <Sidebar></Sidebar>
-        <div className='flex-col'>
-            <h1 className='text-4x1 text-white ml-16 mt-12 text-bold'> Pacientes</h1>
+        <div className='flex-col w-full'>
+          <div className='flex w-full items-end'>
+            <h1 className='text-4x1 text-white pl-16 pt-12 text-bold'> Pacientes</h1>
+            <input
+                  type="text"
+                  id="username"
+                  className="border rounded-lg px-3 py-2 bg-white w-1/2 ml-16 h-12"
+                  placeholder="Digite o nome do paciente"
+              />
+          </div>
             <div className='ml-16 mt-12'>
                <Paciente></Paciente>
             </div>
--- Frontend/doctorsync/components/paciente.tsx
+++ Frontend/doctorsync/components/paciente.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Paciente() {
   return (
-    <div>
-      <h2>Pacientes</h2>
+    <div className="bg-white w-40 border rounded-lg h-40 flex-col">
+      <div className="bg-primary h-12">
+        <h2>Nome paciente 1</h2>
+      </div>
+      <div className="bg-secundary"></div>
     </div>
-  )
+  );
 }
